bc9d7908d3fd62d4cb0dc11703231c0936a553b9
D-damasus <113719811+D-damasus@users.noreply.github.com>
2023-02-06 21:03:42 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,44 @@
-a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
+#!/usr/bin/python3
+"""a class"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        elif value < 1:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return '[Rectangle] {:d}/{:d}'.format(self.__width, self.__height)
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """Square that inherits from Rectangle"""
     def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
